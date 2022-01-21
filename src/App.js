327f2a6216c9bd97fe15aabfccfdccf7ba76fd8a
Hung Vu <hungvsme@gmail.com>
2022-01-20 22:38:02 -0600

    implement simple 360 view from krpano

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const script = document.createElement("script");
+  script.innerHTML = `embedpano({xml:"tour.xml", target:"pano", html5:"only", mobilescale:1.0, passQueryParameters:"startscene,startlookat"});`;
+  useEffect(() => {
+    document.getElementById("pano").appendChild(script);
+  });
   return (
     <div className="App">
+      <div id="pano"></div>
     </div>
   );
 }
